e1bcab75beed516eddcbc4e5f64944feaaaf9611
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2025-01-15 20:25:26 +0000

    feat: add validators

--- src/main/java/spring_boot_to_do_list/spring_boot_to_do_list/infra/http/controllers/TaskController.java
+++ src/main/java/spring_boot_to_do_list/spring_boot_to_do_list/infra/http/controllers/TaskController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 import spring_boot_to_do_list.spring_boot_to_do_list.application.dtos.useCases.task.create.CreateTaskUseCaseInputDto;
 import spring_boot_to_do_list.spring_boot_to_do_list.application.exceptions.BusinessException;
 import spring_boot_to_do_list.spring_boot_to_do_list.application.useCases.task.CreateTaskUseCase;
 import spring_boot_to_do_list.spring_boot_to_do_list.infra.helpers.BaseResponse;
+import spring_boot_to_do_list.spring_boot_to_do_list.infra.http.validators.task.CreateTaskValidator;
 
 @RestController
 @RequestMapping("/tasks")
@@ -22,9 +25,9 @@ public class TaskController {
     private CreateTaskUseCase createTaskUseCase;
 
     @PostMapping
-    public ResponseEntity<Map<String, Object>> create(@RequestBody CreateTaskUseCaseInputDto input) {
+    public ResponseEntity<Map<String, Object>> create(@Valid @RequestBody CreateTaskValidator input) {
         try {
-            this.createTaskUseCase.run(input);
+            this.createTaskUseCase.run(new CreateTaskUseCaseInputDto(input.title, input.description));
             return BaseResponse.success("Task created successfully", HttpStatus.CREATED);
         } catch (BusinessException exception) {
             String errorMessage = exception.getMessage();
---
+++ src/main/java/spring_boot_to_do_list/spring_boot_to_do_list/infra/http/validators/task/CreateTaskValidator.java
@@ -0,0 +1,14 @@
+package spring_boot_to_do_list.spring_boot_to_do_list.infra.http.validators.task;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class CreateTaskValidator {
+    @NotEmpty(message = "Title is required")
+    @Size(max = 100, message = "Title must be less than 100 characters")
+    public String title;
+
+    @NotEmpty(message = "Description is required")
+    @Size(max = 255)
+    public String description;
+}
