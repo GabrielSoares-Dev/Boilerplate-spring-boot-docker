59f7f9840653b59bbe8d1d6ee115c6b0aaba3389
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2025-01-15 09:31:21 -0300

    feat:build project

---
+++ src/test/java/spring_boot_to_do_list/spring_boot_to_do_list/SpringBootToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_to_do_list.spring_boot_to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
