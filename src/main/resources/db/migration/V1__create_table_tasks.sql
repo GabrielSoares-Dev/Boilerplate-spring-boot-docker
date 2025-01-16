76b1103bc8d26a8850210c3f9cb16c2a4c2c01f5
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-01-16 14:00:00 +0000

    feat: add migrations

---
+++ src/main/resources/db/migration/V1__create_table_tasks.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tasks (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    title NVARCHAR(100) NOT NULL,
+    description NVARCHAR(255) NOT NULL,
+    status NVARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    created_at DATETIME NOT NULL DEFAULT GETDATE(),
+    updated_at DATETIME NULL
+);
