4baa06e03f4c5b40b0ba4b93726d479638feb1d2
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-01-16 19:32:37 +0000

    fix: adjust logging

--- src/main/java/spring_boot_to_do_list/spring_boot_to_do_list/infra/models/Task.java
+++ src/main/java/spring_boot_to_do_list/spring_boot_to_do_list/infra/models/Task.java
@@ -8,8 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 import spring_boot_to_do_list.spring_boot_to_do_list.domain.enums.TaskStatus;
 import jakarta.persistence.EnumType;
 
@@ -18,29 +16,62 @@ import jakarta.persistence.EnumType;
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
+
     private Integer id;
 
     @Column(nullable = false, length = 100)
-    @Setter
-    @Getter
     private String title;
 
     @Column(nullable = false, length = 255)
-    @Setter
-    @Getter
     private String description;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    @Setter
-    @Getter
     private TaskStatus status = TaskStatus.PENDING;
 
     @Column(name = "created_at", nullable = false, updatable = false)
-    @Getter
     private LocalDateTime createdAt = LocalDateTime.now();
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
