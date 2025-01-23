2760102265f638f6d909d55bb0b64a89053f8af9
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-01-23 17:50:50 +0000

    feat: add swagger documentation

--- src/main/java/spring_boot_base/spring_boot_base/infra/http/controllers/HelloController.java
+++ src/main/java/spring_boot_base/spring_boot_base/infra/http/controllers/HelloController.java
@@ -1,5 +1,12 @@
 package spring_boot_base.spring_boot_base.infra.http.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +22,7 @@ import spring_boot_base.spring_boot_base.infra.helpers.BaseResponse;
 
 @RestController
 @RequestMapping("/v1/hello")
+@Tag(name = "Hello API", description = "Endpoints for Hello operations")
 public class HelloController {
   @Autowired private LoggerServiceInterface loggerService;
 
@@ -22,6 +30,20 @@ public class HelloController {
 
   @Autowired private HelloUseCase helloUseCase;
 
+  @Operation(summary = "Say hello", description = "Returns a greeting message")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Success",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Map.class),
+                    examples =
+                        @ExampleObject(
+                            value = "{\"message\": \"Hello Example!\", \"statusCode\": 200}"))),
+      })
   @GetMapping("/{name}")
   public ResponseEntity<Map<String, Object>> hello(@PathVariable String name) {
     this.loggerService.debug(String.format("Start %s hello with input: ", this.logContext), name);
